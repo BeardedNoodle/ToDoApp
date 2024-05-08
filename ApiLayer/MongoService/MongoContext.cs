38915ff04d1600151314c90a6fef63e50559ee35
Mert Kurt <mert.kurt1994@gmail.com>
2024-05-08 19:12:25 +0300

    Update

--- ApiLayer/MongoService/MongoContext.cs
+++ ApiLayer/MongoService/MongoContext.cs
@@ -10,20 +10,17 @@ public class MongoDbContext
     public MongoDbContext(IConfiguration configuration)
     {
 
-        
-        var mongoPassword = "B45C5NlkJdRSn5Js"; //Environment.GetEnvironmentVariable("MongoDb_Password");
-        
-        var connectionString = configuration.GetConnectionString("MongoDB")!.Replace("${MongoDb_Password}", mongoPassword);
+        var connectionString = configuration.GetConnectionString("MongoDB");
         var mongoClient = new MongoClient(connectionString);
         _database = mongoClient.GetDatabase("TODO");
     }
 
     public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
-    
-    public IMongoCollection<List> Lists => _database.GetCollection<List>("Users");
-    
-    public IMongoCollection<ListItem> ListItems => _database.GetCollection<ListItem>("Users");
-    
+
+    public IMongoCollection<List> Lists => _database.GetCollection<List>("List");
+
+    public IMongoCollection<ListItem> ListItems => _database.GetCollection<ListItem>("ListItems ");
+
     public IMongoCollection<Follower> Followers => _database.GetCollection<Follower>("Users");
 
 }
